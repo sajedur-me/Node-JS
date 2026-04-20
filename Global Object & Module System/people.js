3d35928cc2f2001892818d3adcac491c4e363390
sajedur-me <sajedur.me@gmail.com>
2026-04-20 18:02:36 +0600

    Module JS file export & import or require file

---
+++ Global Object & Module System/people.js
@@ -0,0 +1,9 @@
+const people = ["Tamim", "Sajed", "Shahriar", "Sabbir"]; // পিপল নামক একটি অ্যারে তৈরি করা হচ্ছে যার মধ্যে চারটি নাম রয়েছে
+
+const number = 123;
+
+const add = (a, b) => {
+    console.log(a + b);
+} 
+
+module.exports = { people }; // মডিউল হিসেবে বিভিন্ন ভেরিয়েবল এক্সপোর্ট করা হচ্ছে 
