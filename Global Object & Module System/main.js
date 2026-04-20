e873054587c6991666eb4d697ff5666936e91033
sajedur-me <sajedur.me@gmail.com>
2026-04-20 21:13:11 +0600

    Lodash library using last function call

--- Global Object & Module System/main.js
+++ Global Object & Module System/main.js
@@ -1,3 +1,7 @@
-const people = require("./people");
+const _ = require("lodash"); // lodash লাইব্রেরি ইমপোর্ট করা হচ্ছে
+const people = require("./people"); // people.js ফাইল থেকে people ভেরিয়েবল ইমপোর্ট করা হচ্ছে
 
 console.log(people);
+console.log(_.last(people.people)); // lodash লাইব্রেরির last ফাংশন ব্যবহার করে people অ্যারের শেষ উপাদানটি প্রিন্ট করা হচ্ছে
+
+
